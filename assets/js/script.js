d621cd8096c15fcc6bef79f4848360055f3dec32
kstensing <stensingk@gmail.com>
2022-01-23 21:14:59 -0600

    created pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
-var rightNow = moment().format('MMMM Do YYYY');
+var currentTime = moment().format('MMMM Do YYYY');
+var task = {};
 
-$(document).ready(function(){
-    $("#currentDay").text(rightNow);        
- 
-});
+
+// $(document).ready(function(){
+//     $("#currentDay").text(currentTime);        
+// });
+
+
+var createTask = function(taskInput) {
+    var taskP = $("<p>")
+    .addClass("task-item")
+    .text(taskInput);
+
+    taskP.append()
+}
+
+var saveTask = function() {
+    $(".saveBtn").click() {
+        localStorage.setItem("tasks", JSON.stringify(task));
+    }
+}
+
+// var loadTask = function() {
+
+// }
+
+// var checkTime = function() {
+//     moment(currentTime).isAfter(currentTime) {
+//         $(
+//     }
+
+// }
